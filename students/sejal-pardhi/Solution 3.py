6ceced0df9b944f24ca87b0a1ddfa912889682c6
sejalpardhi959-crypto <sejalpardhi959@gmail.com>
2026-06-05 11:58:54 +0530

    Create Solution 3.py

---
+++ students/sejal-pardhi/Solution 3.py
@@ -0,0 +1,19 @@
+s = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+ count=0
+ for x in s:
+     if x not in['a','e','i','o', 'u]'
+         count=count+1
+         print("no.of consonant=",conunt)
+
+#problem2
+s = "abcdefghijklmnopqrstuvwxyz"
+
+
+vowels = "aeiouAEIOU"
+
+found_vowels = [char for char in s if char in vowels]
+
+
+print(found_vowels)
+
+
