add782eff8f6ad0ac403445467bf13baf5844cf2
krushnahekare9-cmd <krushnahekare9@gmail.com>
2026-06-09 11:47:16 +0530

    Solution 5.py while problem

---
+++ students/krushna-hekare/Solution 5.py
@@ -0,0 +1,21 @@
+ #problem 1
+num = 10 
+while True:
+    guess = int(input("Guess a number between 1 and 20: "))
+    if guess == num:
+        print("Right")
+    
+    else:
+        print("Wrong")
+
+   #problem 2
+  num = 10
+
+while True:
+    guess = int(input("Guess a number between 1 and 20: "))
+
+    if guess == num:
+        print("Right")
+        break
+    else:
+        print("Wrong")
